fc8dbe4620b05c219cf3cd342793ca40cd82f4c4
SkiesXR <pkrasnick@gmail.com>
2021-01-09 20:39:13 -0800

    Reorganize

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// create a letter and have it transition from white to green
-const letter = document.createElement('span')
-letter.innerText = 'a'
-letter.className = 'animated'
+// 'container' element wraps all the body content
 const container = document.getElementById('container')
 
-// Delay the letter being added to the page
-setTimeout(() => container.appendChild(letter), 3000)
+// create a letter and have it transition from white to green
+const createAndDestroyLetter = () => {
+  const letter = document.createElement('span')
+  letter.innerText = 'a'
+  letter.setAttribute('id', 'a')
+  letter.className = 'animated'
+  setTimeout(() => container.appendChild(letter), 1000)
+}
+
+// Remove the letter from the page
+const removeLetter = () => setTimeout(() => document.getElementById('a').remove(), 3000)
+
+createAndDestroyLetter()
