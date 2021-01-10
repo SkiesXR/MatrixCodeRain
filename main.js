e7777d745a9274358916dae3c9dc28d7a6926bf3
SkiesXR <pkrasnick@gmail.com>
2021-01-09 21:03:07 -0800

    Add English alphabet, create and destroy all characters

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// 'container' element wraps all the body content
-const container = document.getElementById('container')
+const container = document.getElementById('container')  // 'container' element wraps all the body content
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
 
 // create a letter. Have it transition from white to green, then remove it from the DOM
-const createAndDestroyLetter = async () => {
-  const letter = createLetter()
+const createAndDestroyLetter = async (char) => {
+  const letter = createLetter(char)
 
   try {
     await addLetter(letter)
@@ -14,10 +14,10 @@ const createAndDestroyLetter = async () => {
 }
 
 // Create the letter element
-const createLetter = () => {
+const createLetter = (char) => {
   const letter = document.createElement('span')
-  letter.innerText = 'a'
-  letter.setAttribute('id', 'a')
+  letter.innerText = `${char}`
+  letter.setAttribute('id', `${char}`)
   letter.className = 'animated'
   return letter
 }
@@ -25,4 +25,6 @@ const createLetter = () => {
 const addLetter = (letter) => new Promise((resolve, reject) => setTimeout(() => resolve(container.appendChild(letter)), 1000))
 const removeLetter = (letter) => new Promise((resolve, reject) => setTimeout(() => resolve(letter.remove()), 3000))
 
-createAndDestroyLetter()
+for (const char of alphabet) {
+  createAndDestroyLetter(char)
+}
