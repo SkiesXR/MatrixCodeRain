f9545468c496cf6ecd478c0d6df30398c206e699
SkiesXR <pkrasnick@gmail.com>
2021-01-09 20:14:54 -0800

    Add project skeleton and initial styles

---
+++ main.js
@@ -0,0 +1,5 @@
+const letter = document.createElement('span')
+letter.innerText = 'a'
+letter.className = 'animated'
+const container = document.getElementById('container')
+container.appendChild(letter)
