00137d5b0d2f9a4f8799ac09b82d29de2ed547e5
SkiesXR <pkrasnick@gmail.com>
2021-01-09 23:37:37 -0800

    Randomize creation of containers, create constants, re-organize code

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+const containerWidth = 15  // width (in pixels) of each container
+const characterHeight = 18 // height (in pixels) of each character
+const characterCreationDelay = 100 // delay in milliseconds when creating new characters
+const numContainers = window.innerWidth / containerWidth  // number of vertical containers of Matrix code
+const numVerticalCharacters = window.innerHeight / characterHeight  // number of characters to display in each container
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')  // characters to display
+
+// Fisher-Yates shuffle
+const shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 // Create the letter element, have it animate from white to green and then remove opacity
 const createLetter = (char) => {
@@ -12,35 +37,45 @@ const createLetter = (char) => {
 // Add & remove letter from DOM
 const createAndDestroyLetter = (char, containerIndex) => {
   const letter = createLetter(char)
-  setTimeout(() => {
-    document.getElementById(`container-${containerIndex}`).appendChild(letter)
-  }, 1000)
+  setTimeout(() => document.getElementById(`container-${containerIndex}`).appendChild(letter), 1000)
   setTimeout(() => letter.classList.add('invisible'), 10000)
 }
 
 const createContainer = (containerIndex) => {
   const container = document.createElement('div')
-  container.setAttribute('id', `container-${containerIndex}`)
   container.className = 'container'
+  container.setAttribute('id', `container-${containerIndex}`)
   document.body.appendChild(container)
 }
 
-// Iterate through length of alphabet creating & destroying random chars
+// Iterate through length of container creating & destroying random chars
 const fillContainer = (containerIndex) => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < numVerticalCharacters; i++) {
     const idx = Math.floor(Math.random() * alphabet.length)
     setTimeout(() => {
       createAndDestroyLetter(alphabet[idx], containerIndex)
-    }, i * 100)
+    }, i * characterCreationDelay)
   }
 }
 
-for (let i = 0; i < 100; i++) {
-  setTimeout(() => {
+
+const createContainers = () => {
+  const containers = []
+  for (let i = 0; i < numContainers; i++) {
     createContainer(i)
-    fillContainer(i)
-    setTimeout(() => document.getElementById(`container-${i}`).remove(), 15000)
-  }, i * 1000)
+    containers.push(i)
+  }
+
+  return containers
 }
+
+(function createMatrixCode () {
+  const containerIndexes = createContainers()
+  const shuffledContainerIndexes = shuffle(containerIndexes)
+  
+  shuffledContainerIndexes.forEach((containerIndex, i) => {
+    setTimeout(() => fillContainer(containerIndex), i * 1000)
+  })
+})()
 
 
